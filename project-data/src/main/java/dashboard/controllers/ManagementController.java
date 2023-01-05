bcdecf44ca64599d2923bb6615de17867c906bf2
menkaix <mendrika.irina@gmail.com>
2023-01-05 21:50:26 +0300

    what was this import management contrller

--- project-data/src/main/java/dashboard/controllers/ManagementController.java
+++ project-data/src/main/java/dashboard/controllers/ManagementController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
-
 import dashboard.data.dto.ProjectMinimumDTO;
 import dashboard.data.dto.TaskDTO;
 import dashboard.data.entities.People;
